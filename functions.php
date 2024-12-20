0c4fdfeb5ea00126e83bc63a52bc7a8211fa8228
Md. Shohag Rana <cseshohag1275@gmail.com>
2024-12-20 20:06:11 +0600

    wp-supports

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+add_theme_support( 'menus');
+add_theme_support( 'widgets');
+add_theme_support( 'custom-header');
+add_theme_support( 'custom-background');
+add_theme_support( 'custom-logo');
+add_theme_support( 'post-formats',['video','Audio','Gallery']);
+add_theme_support( 'post-thumbnail');
+add_theme_support( 'tag-title');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
